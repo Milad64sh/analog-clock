2d13105382b3feb6f7c7db10b73077cc715e88ea
Milad64sh <m.shalikarian@gmail.com>
2022-08-09 17:20:46 -0700

    js

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const secondHand = document.querySelector('[data-second-hand]');
 const minuteHand = document.querySelector('[data-minute-hand]');
 const hourHand = document.querySelector('[data-hour-hand]');
 
-const currDate = new Date();
+setInterval(() => {
+  const currDate = new Date();
 
-const secDeg = (currDate.getSeconds() / 60) * 360;
-secondHand.style.transform = `rotate(20deg)`;
-const minDeg = (currDate.getSeconds() / 60) * 360;
-minuteHand.style.transform = `rotate(30deg)`;
-const hrDeg = (currDate.getSeconds() / 12) * 360;
-hourHand.style.transform = `rotate(20deg)`;
+  const secDeg = (currDate.getSeconds() / 60) * 360;
+  const minDeg = (currDate.getMinutes() / 60) * 360;
+  const hourDeg = (currDate.getHours() / 12) * 360;
+  secondHand.style.transform = `rotate(${secDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minDeg}deg)`;
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+}, 1000);
